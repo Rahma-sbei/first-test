5b96eb70fdc23a3449817befe6322faa4b5c5f68
rahma <rahma.sbei@medtech.tn>
2024-06-05 10:02:11 +0100

    final program

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,51 @@
  use std::io;
+ 
 
-  fn display(vec:Vec <String>){
-     for i in 0..vec.len(){
-         println!("the element number {} is : {}",i,vec[i]);
+ struct Person{
+    name: String,
+    phone: i64,
+    email:String
+ }
+
+  fn display(v:&mut Vec <Person>){
+     for i in 0..v.len(){
+         println!("the element number {} is : {}",i,v[i].name);
      } 
  }
 
- fn add(v:&mut Vec<String>){
+ fn add(v:&mut Vec <Person>){
     println!("Enter a name to add to the list: " );
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    v.push(input.to_string());
-    println!("Item added successfully: " );
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+    
+    println!("Enter the phone number of this person: " );
+    let mut ph = String::new();
+    io::stdin().read_line(&mut ph).expect("Failed to read line");
+    let pho:i64 = ph.trim().parse().expect("Invalid input");
+    
+    println!("Enter the email number of this person: " );
+    let mut mail = String::new();
+    io::stdin().read_line(&mut mail).expect("Failed to read line");
 
-}
+    let new_person = Person {
+        name: n,
+        phone:pho,
+        email: mail,
+    };
 
-fn remove_contact(v:&mut Vec<String>){
+    
+    v.push(new_person);
+    println!("person added successfully: " );
+
+ }
+
+fn remove_contact(v:&mut Vec <Person>){
     println!("Enter a name to remove from the list: " );
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let mut index:Option<usize> = None ;
     for i in 0..v.len(){
-        if v[i].trim() == input.trim(){
+        if v[i].name.trim() == input.trim(){
             println!("found it ");
             index=Some(i);
             break;
@@ -30,7 +54,10 @@ fn remove_contact(v:&mut Vec<String>){
     match index {
         Some(i) => {
             v.remove(i);
-            println!("Element removed. Vector is now: {:?}", v);
+            println!("Element removed. Vector is now:");
+            for i in 0..v.len(){
+                println!("{}",v[i].name);
+            }
         }
         None => {
             println!("Name invalid or does not exist.");
@@ -39,30 +66,83 @@ fn remove_contact(v:&mut Vec<String>){
     }
 }
 
+fn display_details(v:&mut Vec <Person>){
+    println!("Enter a name to display their details: " );
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let mut index:Option<usize> = None ;
+    for i in 0..v.len(){
+        if v[i].name.trim() == input.trim(){
+            println!("found it ");
+            index=Some(i);
+            break;
+        }
+    } 
+
+    match index {
+        Some(i) => {
+            println!("This contact is:\n *name:{}.\n *phone:{}.\n *email:{}.\n",v[i].name,v[i].phone,v[i].email);
+        }
+        None => {
+            println!("Name invalid or does not exist.");
+        }
+    }
+}
+    
+
+
+
+
+
+
+
+
 fn main() {
     
-    let mut v:Vec <String> = Vec::new();
-    v.push("rahma".to_string());
-    v.push("mey".to_string());
-    v.push("youssef".to_string());
+    let mut v:Vec <Person> = Vec::new();
+     v.push(Person{
+        name: String::from("rahma"),
+        phone: 4569832,
+        email:String::from("rahma@medtech.tn"),
+     });
+     v.push(Person{
+        name: String::from("mey"),
+        phone: 7524131,
+        email:String::from("mey@medtech.tn"),
+     });
+     v.push(Person{
+        name: String::from("youssef"),
+        phone: 9632584,
+        email:String::from("youssef@medtech.tn"),
+     });
     
-    
-        println!("to perform a task click on its corresponding number:\n *to display list : 1.\n *to add contact: 2.\n *to remove contact: 3. " );
+    loop{
+        println!("to perform a task click on its corresponding number:\n *to display list : 1.\n *to add contact: 2.\n *to remove contact: 3.\n *to display details of a contact: 4.\n click anywhere else to exit the program. " );
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
-        let choice:i32 = input.trim().parse().expect("Invalid input");
-
+        
+        let choice: i32 = match input.trim().parse() {
+            Ok(numbr) => numbr,
+            Err(_) => {
+                println!("Exiting program..");
+                break;
+            }
+        };
         match choice {
             
-            1 => display(v),
+            1 => display(&mut v),
             2 => add(&mut v),
             3 => remove_contact(&mut v),
-            _ => println!("Invalid input."),
+            4 => display_details(&mut v),
+            _ => {
+                println!("Exiting the program..");
+                break; 
+            },
         
         }
     
+    }
+} 
     
-   
-    
-}
+
  
